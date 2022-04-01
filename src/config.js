3ca047f3ced06784db602ec31b90f4c61f63e58b
Leonardo Rosales <leonardorosales1485@gmail.com>
2022-04-01 16:37:49 -0300

    enserio

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   api:
-    process.env.NODE_ENV === 'production' ? 'https://cahesbe.herokuapp.com/' : 'https://cahesbe.herokuapp.com/'
+    process.env.NODE_ENV === 'production' ? 'https://cahesbe.herokuapp.com' : 'https://cahesbe.herokuapp.com'
 }
